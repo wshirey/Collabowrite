dc48e0ceec22dd92ef8f378985bb52a9527056e7
wshirey <waltshirey@gmail.com>
2015-07-15 11:29:14 -0500

    - add separate names for server and client side draw() signalr functions - subscribe to fabric's object:added event instead of path:created for future use - put lock around added remote draws so no object:added event is triggered

--- Collabowrite/Scripts/Collabowrite/collabowrite.js
+++ Collabowrite/Scripts/Collabowrite/collabowrite.js
@@ -6,13 +6,25 @@
     canvas.height = document.body.clientHeight;
 
     var draw = $.connection.drawHub;
-    draw.client.draw = function (json) {
+    var send = true;
+    draw.client.drawObject = function (json) {
         //canvas.add(JSON.parse(json));
+        var object = JSON.parse(json);
+        fabric.util.enlivenObjects([JSON.parse(json)], function (objects) {
+            send = false;
+            var origRenderOnAddRemove = canvas.renderOnAddRemove;
+            canvas.renderOnAddRemove = false;
+
+            canvas.add(objects[0]);
+            canvas.renderOnAddRemove = origRenderOnAddRemove;
+            canvas.renderAll();
+            send = true;
+        });
     };
     $.connection.hub.start().done(function () {
-        canvas.on('path:created', function (object) {
-            var json = JSON.stringify(object);
-            draw.server.draw(JSON.stringify(object));
+        canvas.on('object:added', function (object) {
+            var json = JSON.stringify(object.target);
+            if (send) draw.server.objectAdded(JSON.stringify(object.target));
         });
     });
 });
