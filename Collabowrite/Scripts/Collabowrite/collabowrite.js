01e6c5a801844d5ae440ccec8b41bf13191fc49b
wshirey <waltshirey@gmail.com>
2015-07-15 13:04:11 -0500

    locally cache drawing objects and serve to new clients on page load

--- Collabowrite/Scripts/Collabowrite/collabowrite.js
+++ Collabowrite/Scripts/Collabowrite/collabowrite.js
@@ -10,18 +10,31 @@
     draw.client.drawObject = function (json) {
         //canvas.add(JSON.parse(json));
         var object = JSON.parse(json);
-        fabric.util.enlivenObjects([JSON.parse(json)], function (objects) {
+        var objects = [];
+        if (object instanceof Array === false) {
+            objects = [object];
+        } else {
+            if (object.length == 0) return;
+            for (var i = 0; i < object.length; i++)
+            {
+                objects.push(JSON.parse(object[i]));
+            }
+        }
+        fabric.util.enlivenObjects(objects, function (objects) {
             send = false;
             var origRenderOnAddRemove = canvas.renderOnAddRemove;
             canvas.renderOnAddRemove = false;
 
-            canvas.add(objects[0]);
+            for (var i = 0; i < objects.length; i++) {
+                canvas.add(objects[i]);
+            }
             canvas.renderOnAddRemove = origRenderOnAddRemove;
             canvas.renderAll();
             send = true;
         });
     };
     $.connection.hub.start().done(function () {
+        draw.server.loadObjects();
         canvas.on('object:added', function (object) {
             var json = JSON.stringify(object.target);
             if (send) draw.server.objectAdded(JSON.stringify(object.target));
